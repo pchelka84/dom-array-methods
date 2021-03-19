594ca67ad0e15a455aa3e14733f149bb29ec39ee
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-19 08:52:32 -0400

    Select elemens, generate random users

---
+++ script.js
@@ -0,0 +1,28 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionersBtn = document.getElementById('show-millioners');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+
+// Fetch random user and add money
+async function getRandomUser()  {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+  console.log(data);
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random()*1000000)
+  }
+  console.log(newUser)
+}
